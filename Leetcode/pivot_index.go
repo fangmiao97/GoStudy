273bfbf75e927ad8eadf258356e096658ff6716b
Fang Miao <miaofang97@gmail.com>
2022-04-13 23:57:46 +0800

    add search insert

--- Leetcode/pivot_index.go
+++ Leetcode/pivot_index.go
@@ -11,6 +11,7 @@ func pivotIndex(nums []int) int {
 	}
 	leftSum := 0
 	for i, v := range nums {
+		// 如果左边和等于右边，那么这个数就是pivot
 		if leftSum == (sum-v)/2 && (sum-v)%2 == 0 {
 			pivotIndex = i
 			break
